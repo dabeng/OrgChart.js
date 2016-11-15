90ece0c433c3fc4373ac85a5611a682f3dc358bb
dabeng <dabeng413@gmail.com>
2016-11-15 11:22:08 +0800

    Mid: start off example ajax-datasource

--- examples/ajax-datasource/scripts.js
+++ examples/ajax-datasource/scripts.js
@@ -1,45 +1,42 @@
 'use strict';
 
-(function($){
+document.addEventListener('DOMContentLoaded', function () {
 
-  $(function() {
-
-    $.mockjax({
-      url: '/orgchart/initdata',
-      responseTime: 1000,
-      contentType: 'application/json',
-      responseText: {
-        'name': 'Lao Lao',
-        'title': 'general manager',
-        'children': [
-          { 'name': 'Bo Miao', 'title': 'department manager' },
-          { 'name': 'Su Miao', 'title': 'department manager',
-            'children': [
-              { 'name': 'Tie Hua', 'title': 'senior engineer' },
-              { 'name': 'Hei Hei', 'title': 'senior engineer',
-                'children': [
-                  { 'name': 'Pang Pang', 'title': 'engineer' },
-                  { 'name': 'Xiang Xiang', 'title': 'UE engineer' }
-                ]
-              }
-            ]
-          },
-          { 'name': 'Yu Jie', 'title': 'department manager' },
-          { 'name': 'Yu Li', 'title': 'department manager' },
-          { 'name': 'Hong Miao', 'title': 'department manager' },
-          { 'name': 'Yu Wei', 'title': 'department manager' },
-          { 'name': 'Chun Miao', 'title': 'department manager' },
-          { 'name': 'Yu Tie', 'title': 'department manager' }
-        ]
-      }
-    });
-
-    $('#chart-container').orgchart({
-      'data' : '/orgchart/initdata',
-      'depth': 2,
-      'nodeContent': 'title'
-    });
-
+  $.mockjax({
+    url: '/orgchart/initdata',
+    responseTime: 1000,
+    contentType: 'application/json',
+    responseText: {
+      'name': 'Lao Lao',
+      'title': 'general manager',
+      'children': [
+        { 'name': 'Bo Miao', 'title': 'department manager' },
+        { 'name': 'Su Miao', 'title': 'department manager',
+          'children': [
+            { 'name': 'Tie Hua', 'title': 'senior engineer' },
+            { 'name': 'Hei Hei', 'title': 'senior engineer',
+              'children': [
+                { 'name': 'Pang Pang', 'title': 'engineer' },
+                { 'name': 'Xiang Xiang', 'title': 'UE engineer' }
+              ]
+            }
+          ]
+        },
+        { 'name': 'Yu Jie', 'title': 'department manager' },
+        { 'name': 'Yu Li', 'title': 'department manager' },
+        { 'name': 'Hong Miao', 'title': 'department manager' },
+        { 'name': 'Yu Wei', 'title': 'department manager' },
+        { 'name': 'Chun Miao', 'title': 'department manager' },
+        { 'name': 'Yu Tie', 'title': 'department manager' }
+      ]
+    }
   });
 
-})(jQuery);
+  let  orgchart = new OrgChart({
+    'chartContainer': '#chart-container',
+    'data' : '/orgchart/initdata',
+    'depth': 2,
+    'nodeContent': 'title'
+  });
+
+});
