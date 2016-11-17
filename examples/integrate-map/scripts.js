d46f56a0bbfaec6300bca00172bff27e86db256d
dabeng <dabeng413@gmail.com>
2016-11-17 10:06:49 +0800

    refactor the example integrated with map

--- examples/integrate-map/scripts.js
+++ examples/integrate-map/scripts.js
@@ -1,34 +1,32 @@
 'use strict';
 
-(function($){
+document.addEventListener('DOMContentLoaded', function () {
 
-  $(function() {
-
-    var map = new ol.Map({
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.Stamen({
-            layer: 'watercolor'
-          }),
-          preload: 4
+  let map = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.Stamen({
+          layer: 'watercolor'
         }),
-        new ol.layer.Tile({
-          source: new ol.source.Stamen({
-            layer: 'terrain-labels'
-          }),
-          preload: 1
-        })
-      ],
-      target: 'pageBody',
-      view: new ol.View({
-        center: ol.proj.transform([-87.6297980, 41.8781140], 'EPSG:4326', 'EPSG:3857'),
-        zoom: 10
+        preload: 4
+      }),
+      new ol.layer.Tile({
+        source: new ol.source.Stamen({
+          layer: 'terrain-labels'
+        }),
+        preload: 1
       })
-    });
+    ],
+    target: 'pageBody',
+    view: new ol.View({
+      center: ol.proj.transform([-87.6297980, 41.8781140], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 10
+    })
+  });
 
-    $('body').prepend(map.getViewport());
+  document.body.insertBefore(document.querySelector('#chart-container'), map.getViewport());
 
-    var datascource = {
+  let datascource = {
       'name': 'Lao Lao',
       'title': 'President Office',
       'position': [-87.6297980, 41.8781140],
@@ -42,32 +40,31 @@
         { 'name': 'Chun Miao', 'title': 'HR Dept. ', 'position': [-81.3792360, 28.5383350]},
         { 'name': 'Yu Tie', 'title': 'Marketing Dept.', 'position': [-80.1917900, 25.7616800] }
       ]
-    };
-
-    $('#chart-container').orgchart({
+    },
+    orgchart = new OrgChart({
+      'chartContainer': '#chart-container',
       'data' : datascource,
       'nodeContent': 'title',
-      'createNode': function($node, data) {
-        $node.on('click', function() {
-          var view = map.getView();
-          var duration = 2000;
-          var start = +new Date();
-          var pan = ol.animation.pan({
-            duration: duration,
-            source:  view.getCenter(),
-            start: start
-          });
-          var bounce = ol.animation.bounce({
-            duration: duration,
-            resolution: 4 * view.getResolution(),
-            start: start
-          });
+      'createNode': function(node, data) {
+        node.addEventListener('click', () => {
+          let view = map.getView(),
+            duration = 2000,
+            start = +new Date(),
+            pan = ol.animation.pan({
+              'duration': duration,
+              'source':  view.getCenter(),
+              'start': start
+            }),
+            bounce = ol.animation.bounce({
+              'duration': duration,
+              'resolution': 4 * view.getResolution(),
+              'start': start
+            });
+
           map.beforeRender(pan, bounce);
           view.setCenter(ol.proj.transform(data.position, 'EPSG:4326', 'EPSG:3857'));
         });
       }
     });
 
-  });
-
-})(jQuery);
+});
