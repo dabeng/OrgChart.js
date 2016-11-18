7e996330fd0270a02e0010e11dc06c0df58a62ac
dabeng <dabeng413@gmail.com>
2016-11-18 13:53:19 +0800

    refactor the example pan&zoom

--- examples/pan-zoom/scripts.js
+++ examples/pan-zoom/scripts.js
@@ -1,41 +1,37 @@
 'use strict';
 
-(function($){
+document.addEventListener('DOMContentLoaded', function () {
 
-  $(function() {
-
-    var datascource = {
-      'name': 'Lao Lao',
-      'title': 'general manager',
-      'children': [
-        { 'name': 'Bo Miao', 'title': 'department manager' },
-        { 'name': 'Su Miao', 'title': 'department manager',
-          'children': [
-            { 'name': 'Tie Hua', 'title': 'senior engineer' },
-            { 'name': 'Hei Hei', 'title': 'senior engineer',
-              'children': [
-                { 'name': 'Pang Pang', 'title': 'engineer' },
-                { 'name': 'Xiang Xiang', 'title': 'UE engineer' }
-              ]
-            }
-          ]
-        },
-        { 'name': 'Yu Jie', 'title': 'department manager' },
-        { 'name': 'Yu Li', 'title': 'department manager' },
-        { 'name': 'Hong Miao', 'title': 'department manager' },
-        { 'name': 'Yu Wei', 'title': 'department manager' },
-        { 'name': 'Chun Miao', 'title': 'department manager' },
-        { 'name': 'Yu Tie', 'title': 'department manager' }
-      ]
-    };
-
-    $('#chart-container').orgchart({
-      'data' : datascource,
-      'nodeContent': 'title',
-      'pan': true,
-      'zoom': true
-    });
-
+  let datascource = {
+    'name': 'Lao Lao',
+    'title': 'general manager',
+    'children': [
+      { 'name': 'Bo Miao', 'title': 'department manager' },
+      { 'name': 'Su Miao', 'title': 'department manager',
+        'children': [
+          { 'name': 'Tie Hua', 'title': 'senior engineer' },
+          { 'name': 'Hei Hei', 'title': 'senior engineer',
+            'children': [
+              { 'name': 'Pang Pang', 'title': 'engineer' },
+              { 'name': 'Xiang Xiang', 'title': 'UE engineer' }
+            ]
+          }
+        ]
+      },
+      { 'name': 'Yu Jie', 'title': 'department manager' },
+      { 'name': 'Yu Li', 'title': 'department manager' },
+      { 'name': 'Hong Miao', 'title': 'department manager' },
+      { 'name': 'Yu Wei', 'title': 'department manager' },
+      { 'name': 'Chun Miao', 'title': 'department manager' },
+      { 'name': 'Yu Tie', 'title': 'department manager' }
+    ]
+  },
+  orgchart = new OrgChart({
+    'chartContainer': '#chart-container',
+    'data' : datascource,
+    'nodeContent': 'title',
+    'pan': true,
+    'zoom': true
   });
 
-})(jQuery);
+});
