667c5bfe6dff727adc432e004ac40035553f04b8
dabeng <dabeng413@gmail.com>
2016-11-25 10:59:16 +0800

    refactor the example multiple-layers

--- examples/multiple-layers/scripts.js
+++ examples/multiple-layers/scripts.js
@@ -1,12 +1,6 @@
 'use strict';
 
-(function($){
-
-  $.mockjax({
-    url: '/orgchart/root-node',
-    responseTime: 1000,
-    contentType: 'application/json',
-    responseText: {
+Mock.mock('/orgchart/root-node', {
       'className': 'root-node',
       'name': 'Lao Lao',
       'dept': 'president office',
@@ -16,84 +10,84 @@
         { 'name': 'Hong Miao', 'dept': 'finance dept' },
         { 'name': 'Chun Miao', 'dept': 'HR dept' }
       ]
-    }
-  });
-
-  $.mockjax({
-    url: '/orgchart/asso-rd',
-    responseTime: 1000,
-    contentType: 'application/json',
-    responseText: {
-      'className': 'asso-rd drill-up',
-      'name': 'Su Miao',
-      'dept': 'R&D dept',
-      'children': [
-        { 'name': 'Tie Hua', 'dept': 'backend group' },
-        { 'className': 'drill-down asso-frontend', 'name': 'Hei Hei', 'dept': 'frontend group' }
-      ]
-    }
-  });
-
-  $.mockjax({
-    url: '/orgchart/asso-frontend',
-    responseTime: 1000,
-    contentType: 'application/json',
-    responseText:  {
-      'className': 'asso-frontend drill-up',
-      'name': 'Hei Hei',
-      'dept': 'frontend group',
-      'children': [
-        { 'name': 'Pang Pang', 'dept': 'frontend group' },
-        { 'name': 'Xiang Xiang', 'dept': 'frontend group',
-          'children': [
-            { 'name': 'Xiao Xiao', 'dept': 'frontend group' },
-            { 'name': 'Dan Dan', 'dept': 'frontend group' },
-            { 'name': 'Zai Zai', 'dept': 'frontend group' }
-          ]
-        }
-      ]
-    }
-  });
-
-  function initOrgchart(rootClass) {
-    $('#chart-container').orgchart({
-      'chartClass': rootClass,
-      'data' : '/orgchart/' + rootClass,
-      'nodeContent': 'dept',
-      'createNode': function($node, data) {
-        if ($node.is('.drill-down')) {
-          var assoClass = data.className.match(/asso-\w+/)[0];
-          var drillDownIcon = $('<i>', {
-            'class': 'fa fa-arrow-circle-down drill-icon',
-            'click': function() {
-              $('#chart-container').find('.orgchart:visible').addClass('hidden');
-              if (!$('#chart-container').find('.orgchart.' + assoClass).length) {
-                initOrgchart(assoClass);
-              } else {
-                $('#chart-container').find('.orgchart.' + assoClass).removeClass('hidden');
-              }
-            }
-          });
-          $node.append(drillDownIcon);
-        } else if ($node.is('.drill-up')) {
-          var assoClass = data.className.match(/asso-\w+/)[0];
-          var drillUpIcon = $('<i>', {
-            'class': 'fa fa-arrow-circle-up drill-icon',
-            'click': function() {
-              $('#chart-container').find('.orgchart:visible').addClass('hidden').end()
-                .find('.drill-down.' + assoClass).closest('.orgchart').removeClass('hidden');
-            }
-          });
-          $node.append(drillUpIcon);
-        }
-      }
     });
-  }
 
-  $(function() {
-
-    initOrgchart('root-node');
-
+Mock.mock('/orgchart/asso-rd', {
+    'className': 'asso-rd drill-up',
+    'name': 'Su Miao',
+    'dept': 'R&D dept',
+    'children': [
+      { 'name': 'Tie Hua', 'dept': 'backend group' },
+      { 'className': 'drill-down asso-frontend', 'name': 'Hei Hei', 'dept': 'frontend group' }
+    ]
   });
 
-})(jQuery);
+Mock.mock('/orgchart/asso-frontend', {
+    'className': 'asso-frontend drill-up',
+    'name': 'Hei Hei',
+    'dept': 'frontend group',
+    'children': [
+      { 'name': 'Pang Pang', 'dept': 'frontend group' },
+      { 'name': 'Xiang Xiang', 'dept': 'frontend group',
+        'children': [
+          { 'name': 'Xiao Xiao', 'dept': 'frontend group' },
+          { 'name': 'Dan Dan', 'dept': 'frontend group' },
+          { 'name': 'Zai Zai', 'dept': 'frontend group' }
+        ]
+      }
+    ]
+  });
+
+Mock.setup({ timeout: 1000 });
+
+function closest(el, fn) {
+  return el && (fn(el) && el.id !== 'chart-container' ? el : closest(el.parentNode, fn));
+}
+
+function initOrgchart(rootClass) {
+  let orgchart = new OrgChart({
+    'chartContainer': '#chart-container',
+    'chartClass': rootClass,
+    'data' : '/orgchart/' + rootClass,
+    'nodeContent': 'dept',
+    'createNode': function(node, data) {
+      let chartContainer = document.querySelector('#chart-container');
+
+      if (node.classList.contains('drill-down')) {
+        let drillDownIcon = document.createElement('i'),
+          assoClass = data.className.match(/asso-\w+/)[0];
+
+        drillDownIcon.setAttribute('class', 'fa fa-arrow-circle-down drill-icon');
+        drillDownIcon.addEventListener('click', function() {
+          chartContainer.querySelector('.orgchart:not(.hidden)').classList.add('hidden');
+          let assoChart = chartContainer.querySelector('.orgchart.' + assoClass);
+
+          if (!assoChart) {
+            initOrgchart(assoClass);
+          } else {
+            assoChart.classList.remove('hidden');
+          }
+        });
+        node.appendChild(drillDownIcon);
+      } else if (node.classList.contains('drill-up')) {
+        let drillUpIcon = document.createElement('i'),
+          assoClass = data.className.match(/asso-\w+/)[0];
+
+        drillUpIcon.setAttribute('class', 'fa fa-arrow-circle-up drill-icon');
+        drillUpIcon.addEventListener('click', function() {
+          chartContainer.querySelector('.orgchart:not(.hidden)').classList.add('hidden');
+          closest(chartContainer.querySelector('.drill-down.' + assoClass), (el) => {
+            return el.classList && el.classList.contains('orgchart');
+          }).classList.remove('hidden');
+        });
+        node.appendChild(drillUpIcon);
+      }
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  initOrgchart('root-node');
+
+});
