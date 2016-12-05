c2d97a9caf33d1bd402f0d8c91b20d20021df8e5
dabeng <dabeng413@gmail.com>
2016-12-05 18:32:36 +0800

    Mid: build the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,33 @@
-const gulp = require('gulp');
+var gulp = require('gulp'),
+  eslint = require('gulp-eslint'),
+  babel = require('gulp-babel'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  del = require('del');
 
-gulp.task('font', () => {
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+gulp.task('font', function () {
   return gulp.src([
       'node_modules/font-awesome/fonts/*'
     ])
     .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('lint', function () {
+  return gulp.src(['src/*.js'])
+    .pipe(eslint('.eslintrc.json'))
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('js', ['lint', 'clean'], function () {
+  return gulp.src(['src/*.js'])
+    .pipe(babel(
+      {presets: ['es2015']}
+    ))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/orgchart.min.js'));
 });
